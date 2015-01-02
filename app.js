ff5aecfa0b93d45c82f4f9c930e8f8b889486259
Brenton Klassen <midwestmicro@live.com>
2015-01-01 22:56:01 -0600

    Changed tons of stuff

--- app.js
+++ app.js
@@ -1,16 +1,80 @@
-angular.module('GiltApp', [])
+angular.module('GiltApp', ['ngRoute'])
 
-.controller('GiltController',['$scope','$http','$log',
+.config(function($routeProvider){
+
+	$routeProvider
+		.when('/', {
+			controller: 'SalesController',
+			templateUrl: 'sales.html'
+		})
+		.when('/men', {
+			controller: 'MenSalesController',
+			templateUrl: 'sales.html'
+		})
+		.when('/women', {
+			controller: 'WomenSalesController',
+			templateUrl: 'sales.html'
+		})
+		.when('/about', {
+			templateUrl: 'about.html'
+		})
+		.otherwise({ redirectTo: '/' });
+
+})
+
+.controller('SalesController',['$scope','$http','$log',
 
 	function($scope,$http,$log){
 
 		$scope.$log = $log;
 
-		$scope.items = [];
+		$scope.sales = [];
 		$scope.getItems = function(){
 			$http({method:'GET',url:'https://api.gilt.com/v1/sales/active.json?apikey=31e61f5b3173cc7b9db11bd5441351c319c07999f139cdd368494c62457f0f53'})
 			.success(function(data,status){
-				$scope.items = data.sales;
+				$scope.sales = data.sales;
+			})
+			.error(function(data,status){
+				alert(status);
+			});
+		};
+
+	}
+
+])
+
+.controller('WomenSalesController',['$scope','$http','$log',
+
+	function($scope,$http,$log){
+
+		$scope.$log = $log;
+
+		$scope.sales = [];
+		$scope.getItems = function(){
+			$http({method:'GET',url:'https://api.gilt.com/v1/sales/women/active.json?apikey=31e61f5b3173cc7b9db11bd5441351c319c07999f139cdd368494c62457f0f53'})
+			.success(function(data,status){
+				$scope.sales = data.sales;
+			})
+			.error(function(data,status){
+				alert(status);
+			});
+		};
+
+	}
+
+])
+
+.controller('MenSalesController',['$scope','$http','$log',
+
+	function($scope,$http,$log){
+
+		$scope.$log = $log;
+
+		$scope.sales = [];
+		$scope.getItems = function(){
+			$http({method:'GET',url:'https://api.gilt.com/v1/sales/men/active.json?apikey=31e61f5b3173cc7b9db11bd5441351c319c07999f139cdd368494c62457f0f53'})
+			.success(function(data,status){
+				$scope.sales = data.sales;
 			})
 			.error(function(data,status){
 				alert(status);
